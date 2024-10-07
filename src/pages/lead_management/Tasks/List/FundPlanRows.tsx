c1f66fdd2319a2890b3edd3b0a73a327463afa55
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-10-07 18:25:25 +0530

    added extra fields in fund plan

--- src/pages/lead_management/Tasks/List/FundPlanRows.tsx
+++ src/pages/lead_management/Tasks/List/FundPlanRows.tsx
@@ -9,14 +9,11 @@ const fundTypeOptions = [
   { value: "loan", label: "Loan" },
   { value: "savings", label: "Savings" },
   { value: "fd", label: "FD" },
+  { value: "salary", label: "Salary" },
+  { value: "pf", label: "PF" },
 ];
 
-const FundPlanRows = ({
-  fundPlan,
-  handleFundPlanInputChange,
-  removeFundPlan,
-  handleAddMoreFundPlan,
-}: any) => {
+const FundPlanRows = ({ fundPlan, handleFundPlanInputChange, removeFundPlan, handleAddMoreFundPlan }: any) => {
   console.log(fundPlan);
 
   const renderFundRows = (plan: any, index: number) => (
@@ -29,17 +26,11 @@ const FundPlanRows = ({
             <Select
               name="type"
               placeholder="Select fund type"
-              value={fundTypeOptions.find(
-                (option) => option.value === plan.type
-              )}
-              onChange={(selectedOption: any) =>
-                handleFundPlanInputChange(index, "type", selectedOption?.value)
-              }
+              value={fundTypeOptions.find((option) => option.value === plan.type)}
+              onChange={(selectedOption: any) => handleFundPlanInputChange(index, "type", selectedOption?.value)}
               options={fundTypeOptions}
             />
-            {plan.errors?.type && (
-              <Form.Text className="text-danger">{plan.errors.type}</Form.Text>
-            )}
+            {plan.errors?.type && <Form.Text className="text-danger">{plan.errors.type}</Form.Text>}
           </Form.Group>
         </Col>
 
@@ -52,65 +43,62 @@ const FundPlanRows = ({
               name="sponsor_name"
               placeholder="Enter sponsor name"
               value={plan.sponsor_name || ""}
-              onChange={(e) =>
-                handleFundPlanInputChange(index, e.target.name, e.target.value)
-              }
+              onChange={(e) => handleFundPlanInputChange(index, e.target.name, e.target.value)}
             />
-            {plan.errors?.sponsor_name && (
-              <Form.Text className="text-danger">
-                {plan.errors.sponsor_name}
-              </Form.Text>
-            )}
+            {plan.errors?.sponsor_name && <Form.Text className="text-danger">{plan.errors.sponsor_name}</Form.Text>}
           </Form.Group>
         </Col>
 
         {/* Approx Annual Income */}
-        <Col md={4} lg={4} xl={4} xxl={4}>
-          <Form.Group
-            className="mb-3"
-            controlId={`approx_annual_income-${index}`}
-          >
+        {/* <Col md={4} lg={4} xl={4} xxl={4}>
+          <Form.Group className="mb-3" controlId={`approx_annual_income-${index}`}>
             <Form.Label>Approx Annual Income</Form.Label>
             <FormInput
               type="number"
               name="approx_annual_income"
               placeholder="Enter approx. annual income"
               value={plan.approx_annual_income || ""}
-              onChange={(e) =>
-                handleFundPlanInputChange(index, e.target.name, e.target.value)
-              }
+              onChange={(e) => handleFundPlanInputChange(index, e.target.name, e.target.value)}
               min={0}
             />
             {plan.errors?.approx_annual_income && (
-              <Form.Text className="text-danger">
-                {plan.errors.approx_annual_income}
-              </Form.Text>
+              <Form.Text className="text-danger">{plan.errors.approx_annual_income}</Form.Text>
             )}
           </Form.Group>
-        </Col>
+        </Col> */}
 
         {/* Relation with sponsor*/}
 
         <Col md={4} lg={4} xl={4} xxl={4}>
-          <Form.Group
-            className="mb-3"
-            controlId={`relation_with_sponsor-${index}`}
-          >
+          <Form.Group className="mb-3" controlId={`relation_with_sponsor-${index}`}>
             <Form.Label>Relation with sponsor</Form.Label>
             <FormInput
               type="text"
               name="relation_with_sponsor"
               placeholder="Enter relation with sponsor"
               value={plan.relation_with_sponsor || ""}
-              onChange={(e) =>
-                handleFundPlanInputChange(index, e.target.name, e.target.value)
-              }
+              onChange={(e) => handleFundPlanInputChange(index, e.target.name, e.target.value)}
             />
             {plan.errors?.relation_with_sponsor && (
-              <Form.Text className="text-danger">
-                {plan.errors.relation_with_sponsor}
-              </Form.Text>
+              <Form.Text className="text-danger">{plan.errors.relation_with_sponsor}</Form.Text>
             )}
+          </Form.Group>
+        </Col>
+
+        {/* Sponsorship amount */}
+
+        <Col md={4} lg={4} xl={4} xxl={4}>
+          <Form.Group className="mb-3" controlId={`sponsorship_amount-${index}`}>
+            <Form.Label>Sponsorship amount</Form.Label>
+            <FormInput
+              type="number"
+              name="sponsorship_amount"
+              placeholder="Enter sponsorship amount"
+              value={plan.sponsorship_amount || ""}
+              onChange={(e) => handleFundPlanInputChange(index, e.target.name, e.target.value)}
+              min={0}
+            />
+            {plan.errors?.sponsorship_amount && <Form.Text className="text-danger">{plan.errors.sponsorship_amount}</Form.Text>}
           </Form.Group>
         </Col>
 
@@ -124,41 +112,9 @@ const FundPlanRows = ({
               name="name_of_bank"
               placeholder="Enter name of bank"
               value={plan.name_of_bank || ""}
-              onChange={(e) =>
-                handleFundPlanInputChange(index, e.target.name, e.target.value)
-              }
+              onChange={(e) => handleFundPlanInputChange(index, e.target.name, e.target.value)}
             />
-            {plan.errors?.name_of_bank && (
-              <Form.Text className="text-danger">
-                {plan.errors.name_of_bank}
-              </Form.Text>
-            )}
-          </Form.Group>
-        </Col>
-
-        {/* Sponsorship amount */}
-
-        <Col md={4} lg={4} xl={4} xxl={4}>
-          <Form.Group
-            className="mb-3"
-            controlId={`sponsorship_amount-${index}`}
-          >
-            <Form.Label>Sponsorship amount</Form.Label>
-            <FormInput
-              type="number"
-              name="sponsorship_amount"
-              placeholder="Enter sponsorship amount"
-              value={plan.sponsorship_amount || ""}
-              onChange={(e) =>
-                handleFundPlanInputChange(index, e.target.name, e.target.value)
-              }
-              min={0}
-            />
-            {plan.errors?.sponsorship_amount && (
-              <Form.Text className="text-danger">
-                {plan.errors.sponsorship_amount}
-              </Form.Text>
-            )}
+            {plan.errors?.name_of_bank && <Form.Text className="text-danger">{plan.errors.name_of_bank}</Form.Text>}
           </Form.Group>
         </Col>
 
@@ -175,9 +131,7 @@ const FundPlanRows = ({
                 name={`itr_status-${index}`}
                 checked={plan?.itr_status === "yes"} // If "yes", check this button
                 value="yes"
-                onChange={(e: any) =>
-                  handleFundPlanInputChange(index, "itr_status", e.target.value)
-                }
+                onChange={(e: any) => handleFundPlanInputChange(index, "itr_status", e.target.value)}
               />
 
               <Form.Check
@@ -188,9 +142,7 @@ const FundPlanRows = ({
                 name={`itr_status-${index}`}
                 checked={plan?.itr_status === "no"} // If "no", check this button
                 value="no"
-                onChange={(e: any) =>
-                  handleFundPlanInputChange(index, "itr_status", e.target.value)
-                }
+                onChange={(e: any) => handleFundPlanInputChange(index, "itr_status", e.target.value)}
               />
             </div>
           </Form.Group>
@@ -198,27 +150,14 @@ const FundPlanRows = ({
 
         {/* Supporting Document */}
         <Col md={4} lg={4} xl={4} xxl={4}>
-          <Form.Group
-            className="mb-3"
-            controlId={`supporting_document-${index}`}
-          >
+          <Form.Group className="mb-3" controlId={`supporting_document-${index}`}>
             <Form.Label>Supporting Document</Form.Label>
             <Form.Control
               type="file"
               name="supporting_document"
-              onChange={(e: any) =>
-                handleFundPlanInputChange(
-                  index,
-                  e.target.name,
-                  e.target.files?.[0]
-                )
-              }
+              onChange={(e: any) => handleFundPlanInputChange(index, e.target.name, e.target.files?.[0])}
             />
-            {plan.errors?.supporting_document && (
-              <Form.Text className="text-danger">
-                {plan.errors.supporting_document}
-              </Form.Text>
-            )}
+            {plan.errors?.supporting_document && <Form.Text className="text-danger">{plan.errors.supporting_document}</Form.Text>}
             {typeof plan.supporting_document === "string" && (
               <div className="d-flex align-items-center">
                 <i className="mdi mdi-eye text-primary me-2"></i>
@@ -234,6 +173,54 @@ const FundPlanRows = ({
             )}
           </Form.Group>
         </Col>
+        {(plan?.type === "fd" || plan?.type === "savings") && (
+          <>
+            <Col md={4} lg={4} xl={4} xxl={4}>
+              <Form.Group className="mb-3" controlId={`has_min_6_months_backup-${index}`}>
+                <Form.Label className="mb-2">Does the funds have min 6 months has_min_6_months_backup?</Form.Label>
+                <div>
+                  <Form.Check
+                    inline
+                    id={`yes-${index}`}
+                    label="Yes"
+                    type="radio"
+                    name={`has_min_6_months_backup-${index}`}
+                    checked={plan?.has_min_6_months_backup === "yes"} // If "yes", check this button
+                    value="yes"
+                    onChange={(e: any) => handleFundPlanInputChange(index, "has_min_6_months_backup", e.target.value)}
+                  />
+
+                  <Form.Check
+                    inline
+                    label="No"
+                    id={`no-${index}`}
+                    type="radio"
+                    name={`has_min_6_months_backup-${index}`}
+                    checked={plan?.has_min_6_months_backup === "no"} // If "no", check this button
+                    value="no"
+                    onChange={(e: any) => handleFundPlanInputChange(index, "has_min_6_months_backup", e.target.value)}
+                  />
+                </div>
+              </Form.Group>
+            </Col>
+
+            <Col md={4} lg={4} xl={4} xxl={4}>
+              <Form.Group className="mb-3" controlId={`source_of_funds-${index}`}>
+                <Form.Label>Explain the source of funds for FD/Savings.</Form.Label>
+                <Form.Control
+                  // type="text"
+                  as={'textarea'}
+                  rows={3}
+                  name="source_of_funds"
+                  placeholder="Explain the source of funds for FD/Savings."
+                  value={plan.source_of_funds || ""}
+                  onChange={(e) => handleFundPlanInputChange(index, e.target.name, e.target.value)}
+                />
+                {plan.errors?.source_of_funds && <Form.Text className="text-danger">{plan.errors.source_of_funds}</Form.Text>}
+              </Form.Group>
+            </Col>
+          </>
+        )}
         <Row>
           {fundPlan.length > 1 && (
             <ActionButton
@@ -259,11 +246,7 @@ const FundPlanRows = ({
       {fundPlan?.map((plan: any, index: number) => renderFundRows(plan, index))}
 
       <Row className="mb-2">
-        <ActionButton
-          onClick={handleAddMoreFundPlan}
-          label="Add more"
-          iconClass="mdi mdi-plus"
-        />
+        <ActionButton onClick={handleAddMoreFundPlan} label="Add more" iconClass="mdi mdi-plus" />
       </Row>
     </>
   );
