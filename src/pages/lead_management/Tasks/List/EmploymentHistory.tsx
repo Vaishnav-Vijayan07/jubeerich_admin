6af0c9fdc06bf39f1eaf51fbb824898ea9fb3dce
Joshua-Intersmart <Joshua@intersmart.in>
2024-10-08 11:52:44 +0530

    Added Employment Histories
    Added Employment Histories

---
+++ src/pages/lead_management/Tasks/List/EmploymentHistory.tsx
@@ -0,0 +1,396 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Form, Row, Spinner } from 'react-bootstrap'
+import { FormInput } from '../../../../components'
+import { baseUrl, showSuccessAlert } from '../../../../constants'
+import axios from 'axios'
+
+const EmploymentHistory = (props: any) => {
+  const { studentId } = props;
+  const [noticePeriod, setNoticePeriod] = useState<any>(false)
+  const [terminated, setTerminated] = useState<any>(false)
+  const [relation, setRelation] = useState<any>(false)
+  const [forgotDocuments, setForgotDocuments] = useState<any>(false)
+  const [abroadWork, setAbroadWork] = useState<any>(false)
+
+  const [formData, setFormData] = useState({
+    noticePeriod: false,
+    terminated: false,
+    relation: false,
+    forgotDocuments: false,
+    abroadWork: false,
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [documents, setDocuments] = useState({
+    visaPage: '',
+    permitCard: '',
+    salaryAccountStatement: '',
+    supportingDocuments: '',
+  });
+  const [documentsName, setDocumentsName] = useState({
+    visa_page: '',
+    permit_card: '',
+    salary_account_statement: '',
+    supporting_documents: '',
+  });
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const { name } = e.target;
+
+    setDocuments((prevDocs) => ({
+      ...prevDocs,
+      [name]: file,
+    }));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: checked,
+    });
+  };
+
+  const onSubmit = async () => {
+    try {
+      console.log('formData', formData);
+      
+      const formBody = new FormData();
+
+      formBody.append("served_notice_period", JSON.stringify(noticePeriod))
+      formBody.append("terminated_from_company", JSON.stringify(terminated))
+      formBody.append("good_relation_with_employers", JSON.stringify(relation))
+      formBody.append("submitted_forged_documents", JSON.stringify(forgotDocuments))
+      formBody.append("has_abroad_work_evidence", JSON.stringify(abroadWork));
+      formBody.append("visaPage", documents.visaPage)
+      formBody.append("permitCard", documents.permitCard)
+      formBody.append("salaryAccountStatement", documents.salaryAccountStatement)
+      formBody.append("supportingDocuments", documents.supportingDocuments)
+
+      const result = await axios.post(`/employment_history/${studentId}`, formBody, {
+        headers: {
+          "content-type": "multipart/form-data",
+        }
+      });
+
+      if (result) {
+        showSuccessAlert("Employment Data Saved Successffully");
+        getEmploymentHistories();
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getEmploymentHistories = async () => {
+    try {
+      setIsLoading(true);
+
+      const result = await axios.get(`/employment_history/${studentId}`)
+      if (result) {
+        // setFormData(result?.data?.data)
+        setNoticePeriod(result?.data?.data?.served_notice_period);
+        setTerminated(result?.data?.data?.terminated_from_company);
+        setRelation(result?.data?.data?.good_relation_with_employers);
+        setForgotDocuments(result?.data?.data?.submitted_forged_documents);
+        setAbroadWork(result?.data?.data?.has_abroad_work_evidence);
+        setDocumentsName(result?.data?.data);
+        setIsLoading(false);
+      }
+
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getEmploymentHistories()
+  }, [studentId])
+
+  if (isLoading) {
+    return (
+      <Spinner
+        animation="border"
+        style={{ position: "absolute", top: "50%", left: "50%" }}
+      />
+    );
+  }
+
+  return (
+    <>
+      <h5 className="mb-1 mt-4 text-uppercase">
+        <i className="mdi mdi-account-circle me-1"></i>Employment History
+      </h5>
+      <Row className='mt-3'>
+        <Col className='mb-2' md={12} lg={12} xl={12} xxl={6}>
+          <Form.Group className="mb-2" controlId="noticePeriod">
+            <Form.Label>
+              <span className="text-danger">* </span>
+              Have you served notice period as per the requirement of your employer during your last resignation
+            </Form.Label>
+            <Col className="d-flex gap-2">
+              <Form.Check
+                type="radio"
+                id="noticePeriod"
+                label="Yes"
+                checked={noticePeriod}
+                onChange={() => setNoticePeriod(true)}
+                // onChange={handleChange}
+                name="noticePeriod"
+              />
+
+              <Form.Check
+                type="radio"
+                id="noticePeriod"
+                label="No"
+                checked={!noticePeriod}
+                onChange={() => setNoticePeriod(false)}
+                // onChange={handleChange}
+                name="noticePeriod"
+              />
+            </Col>
+          </Form.Group>
+        </Col>
+
+        <Col className='mb-2' md={12} lg={12} xl={12} xxl={6}>
+          <Form.Group className="mb-2" controlId="terminated">
+            <Form.Label>
+              <span className="text-danger">* </span>
+              Did you get terminated from any organization/company
+            </Form.Label>
+            <Col className="d-flex gap-2">
+              <Form.Check
+                type="radio"
+                id="terminated"
+                label="Yes"
+                checked={terminated}
+                onChange={() => setTerminated(true)}
+                // onChange={handleChange}
+                name="terminated"
+              />
+
+              <Form.Check
+                type="radio"
+                id="terminated"
+                label="No"
+                checked={!terminated}
+                onChange={() => setTerminated(false)}
+                // onChange={handleChange}
+                name="terminated"
+              />
+            </Col>
+          </Form.Group>
+        </Col>
+
+        <Col className='mb-2' md={12} lg={12} xl={12} xxl={6}>
+          <Form.Group className="mb-2" controlId="relation">
+            <Form.Label>
+              <span className="text-danger">* </span>
+              Are you in very good and friendly relation with all of your previous/current employers
+            </Form.Label>
+            <Col className="d-flex gap-2">
+              <Form.Check
+                type="radio"
+                id="relation"
+                label="Yes"
+                checked={relation}
+                onChange={() => setRelation(true)}
+                // onChange={handleChange}
+                name="relation"
+              />
+
+              <Form.Check
+                type="radio"
+                id="relation"
+                label="No"
+                checked={!relation}
+                onChange={() => setRelation(false)}
+                // onChange={handleChange}
+                name="relation"
+              />
+            </Col>
+          </Form.Group>
+        </Col>
+
+        <Col className='mb-2' md={12} lg={12} xl={12} xxl={6}>
+          <Form.Group className="mb-2" controlId="forgotDocuments">
+            <Form.Label>
+              <span className="text-danger">* </span>
+              Is there any forged experience or any other documents submitted to us
+            </Form.Label>
+            <Col className="d-flex gap-2">
+              <Form.Check
+                type="radio"
+                id="forgotDocuments"
+                label="Yes"
+                checked={forgotDocuments}
+                onChange={() => setForgotDocuments(true)}
+                // onChange={handleChange}
+                name="forgotDocuments"
+              />
+
+              <Form.Check
+                type="radio"
+                id="forgotDocuments"
+                label="No"
+                checked={!forgotDocuments}
+                onChange={() => setForgotDocuments(false)}
+                // onChange={handleChange}
+                name="forgotDocuments"
+              />
+            </Col>
+          </Form.Group>
+        </Col>
+
+        <Col className='mb-2' md={12} lg={12} xl={12} xxl={6}>
+          <Form.Group className="mb-2" controlId="abroadWork">
+            <Form.Label>
+              <span className="text-danger">* </span>
+              For any abroad work experiences do you still have the visa page, permit card, salary account statement, and all other supporting evidence to prove the same
+            </Form.Label>
+            <Col className="d-flex gap-2">
+              <Form.Check
+                type="radio"
+                id="abroadWork"
+                label="Yes"
+                checked={abroadWork}
+                // onChange={() => setAbroadWork(true)}
+                onChange={handleChange}
+                name="abroadWork"
+              />
+
+              <Form.Check
+                type="radio"
+                id="abroadWork"
+                label="No"
+                checked={!abroadWork}
+                onChange={() => setAbroadWork(false)}
+                // onChange={handleChange}
+                name="abroadWork"
+              />
+            </Col>
+          </Form.Group>
+        </Col>
+
+        <Col md={6} lg={6} xl={6} xxl={4}>
+          <Form.Group className="mb-2" controlId="visaPage">
+            <Form.Label>
+              <span className="text-danger">*</span>
+              Visa Page
+            </Form.Label>
+            <FormInput
+              type="file"
+              name="visaPage"
+              onChange={(e) =>
+                handleFileChange(e)
+              }
+            />
+          </Form.Group>
+          <div className='d-flex mb-2'>
+            {documentsName?.visa_page && <a
+              href={`${baseUrl}/uploads/experienceHistoryDocs/${documentsName?.visa_page}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none border rounded-2 border-1 border-secondary text-truncate"
+            >
+              <div className=' p-1'>
+                {(documentsName?.visa_page ? "Visa Page" : "")}
+              </div>
+            </a>}
+          </div>
+        </Col>
+
+        <Col md={6} lg={6} xl={6} xxl={4}>
+          <Form.Group className="mb-2" controlId="permitCard">
+            <Form.Label>
+              <span className="text-danger">*</span>
+              Permit Card
+            </Form.Label>
+            <FormInput
+              type="file"
+              name="permitCard"
+              onChange={(e) =>
+                handleFileChange(e)
+              }
+            />
+          </Form.Group>
+          <div className='d-flex mb-2'>
+            {documentsName?.permit_card && <a
+              href={`${baseUrl}/uploads/experienceHistoryDocs/${documentsName?.permit_card}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none border rounded-2 border-1 border-secondary text-truncate"
+            >
+              <div className=' p-1'>
+                {(documentsName?.permit_card ? "Permit Card" : "")}
+              </div>
+            </a>}
+          </div>
+        </Col>
+
+        <Col md={6} lg={6} xl={6} xxl={4}>
+          <Form.Group className="mb-2" controlId="salaryAccountStatement">
+            <Form.Label>
+              <span className="text-danger">*</span>
+              Salary Account Statement
+            </Form.Label>
+            <FormInput
+              type="file"
+              name="salaryAccountStatement"
+              onChange={(e) =>
+                handleFileChange(e)
+              }
+            />
+          </Form.Group>
+          <div className='d-flex mb-2'>
+            {documentsName?.salary_account_statement && <a
+              href={`${baseUrl}/uploads/experienceHistoryDocs/${documentsName?.salary_account_statement}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none border rounded-2 border-1 border-secondary text-truncate"
+            >
+              <div className=' p-1'>
+                {(documentsName?.salary_account_statement ? "Salary Account Statement" : "")}
+              </div>
+            </a>}
+          </div>
+        </Col>
+
+        <Col md={6} lg={6} xl={6} xxl={4}>
+          <Form.Group className="mb-2" controlId="supportingDocuments">
+            <Form.Label>
+              <span className="text-danger">*</span>
+              Supporting Documents
+            </Form.Label>
+            <FormInput
+              type="file"
+              name="supportingDocuments"
+              onChange={(e) =>
+                handleFileChange(e)
+              }
+            />
+          </Form.Group>
+          <div className='d-flex mb-2'>
+            {documentsName?.supporting_documents && <a
+              href={`${baseUrl}/uploads/experienceHistoryDocs/${documentsName?.supporting_documents}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none border rounded-2 border-1 border-secondary text-truncate"
+            >
+              <div className=' p-1'>
+                {(documentsName?.supporting_documents ? "Supporting Documents" : "")}
+              </div>
+            </a>}
+          </div>
+        </Col>
+
+        <Button className="mt-1" onClick={onSubmit}>Save Experience History</Button>
+      </Row>
+    </>
+  )
+}
+
+export default EmploymentHistory
