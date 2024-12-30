c035476c5d65be38b3a69dec8b9b9bdf8eacac03
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-12-30 17:37:46 +0530

    fixed logout issue

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -15,7 +15,10 @@ import DetachedLayout from "../layouts/Detached";
 import HorizontalLayout from "../layouts/Horizontal/";
 import TwoColumnLayout from "../layouts/TwoColumn/";
 
-import { authProtectedFlattenRoutes, publicProtectedFlattenRoutes } from ".";
+import {
+  authProtectedFlattenRoutes,
+  publicProtectedFlattenRoutes,
+} from "./index";
 import { APICore } from "../helpers/api/apiCore";
 
 interface IRoutesProps {}
@@ -52,52 +55,38 @@ const AllRoutes = (props: IRoutesProps) => {
     <React.Fragment>
       <Routes>
         <Route>
-          {/* Publically accessible */}
           {publicProtectedFlattenRoutes.map((route, idx) => (
-            <>
-              <Route
-                path={route.path}
-                element={
-                  api.isUserAuthenticated() == true ? (
-                    <Navigate
-                      to={{
-                        pathname: "/",
-                      }}
-                    />
-                  ) : (
-                    <DefaultLayout {...props} layout={layout}>
-                      {route.element}
-                    </DefaultLayout>
-                  )
-                }
-                key={idx}
-              />
-            </>
+            <Route
+              path={route.path}
+              element={
+                <DefaultLayout {...props} layout={layout}>
+                  {route.element}
+                </DefaultLayout>
+              }
+              key={idx}
+            />
           ))}
         </Route>
 
         <Route>
-          {/* Only accessible to authenitcated users accessible */}
-          {authProtectedFlattenRoutes?.map((route, idx) => (
-            <>
-              <Route
-                path={route.path}
-                element={
-                  api.isUserAuthenticated() === false ? (
-                    <Navigate
-                      to={{
-                        pathname: "/auth/login/",
-                        // hash:route.path,
-                        search: "next=" + route.path,
-                      }}
-                    />
-                  ) : (
-                    <Layout {...props}>{route.element}</Layout>
-                  )
-                }
-                key={idx}
-              />
-            </>
+          {authProtectedFlattenRoutes.map((route, idx) => (
+            <Route
+              path={route.path}
+              element={
+                api.isUserAuthenticated() === false ? (
+                  <Navigate
+                    to={{
+                      pathname: "/auth/login",
+                      // hash:route.path,
+                      search: "next=" + route.path,
+                    }}
+                  />
+                ) : (
+                  <Layout {...props}>{route.element}</Layout>
+                )
+              }
+              key={idx}
+            />
           ))}
         </Route>
       </Routes>
@@ -106,3 +95,113 @@ const AllRoutes = (props: IRoutesProps) => {
 };
 
 export default AllRoutes;
+
+
+// import React from "react";
+// import { Navigate, Route, Routes } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+// // layout constants
+// import { LayoutTypes } from "../constants/layout";
+
+// // strore
+// import { RootState } from "../redux/store";
+
+// // All layouts containers
+// import DefaultLayout from "../layouts/Default";
+// import VerticalLayout from "../layouts/Vertical";
+// import DetachedLayout from "../layouts/Detached";
+// import HorizontalLayout from "../layouts/Horizontal/";
+// import TwoColumnLayout from "../layouts/TwoColumn/";
+
+// import { authProtectedFlattenRoutes, publicProtectedFlattenRoutes } from ".";
+// import { APICore } from "../helpers/api/apiCore";
+
+// interface IRoutesProps {}
+
+// const AllRoutes = (props: IRoutesProps) => {
+//   const { layout } = useSelector((state: RootState) => ({
+//     layout: state.Layout,
+//   }));
+
+//   const getLayout = () => {
+//     let layoutCls = TwoColumnLayout;
+
+//     switch (layout.layoutType) {
+//       case LayoutTypes.LAYOUT_HORIZONTAL:
+//         layoutCls = HorizontalLayout;
+//         break;
+//       case LayoutTypes.LAYOUT_DETACHED:
+//         layoutCls = DetachedLayout;
+//         break;
+//       case LayoutTypes.LAYOUT_VERTICAL:
+//         layoutCls = VerticalLayout;
+//         break;
+//       default:
+//         layoutCls = TwoColumnLayout;
+//         break;
+//     }
+//     return layoutCls;
+//   };
+
+//   let Layout = getLayout();
+//   const api = new APICore();
+
+//   return (
+//     <React.Fragment>
+//       <Routes>
+//         <Route>
+//           {/* Publically accessible */}
+//           {publicProtectedFlattenRoutes.map((route, idx) => (
+//             <>
+//               <Route
+//                 path={route.path}
+//                 element={
+//                   api.isUserAuthenticated() == true ? (
+//                     <Navigate
+//                       to={{
+//                         pathname: "/",
+//                       }}
+//                     />
+//                   ) : (
+//                     <DefaultLayout {...props} layout={layout}>
+//                       {route.element}
+//                     </DefaultLayout>
+//                   )
+//                 }
+//                 key={idx}
+//               />
+//             </>
+//           ))}
+//         </Route>
+
+//         <Route>
+//           {/* Only accessible to authenitcated users accessible */}
+//           {authProtectedFlattenRoutes?.map((route, idx) => (
+//             <>
+//               <Route
+//                 path={route.path}
+//                 element={
+//                   api.isUserAuthenticated() === false ? (
+//                     <Navigate
+//                       to={{
+//                         pathname: "/auth/login/",
+//                         // hash:route.path,
+//                         search: "next=" + route.path,
+//                       }}
+//                     />
+//                   ) : (
+//                     <Layout {...props}>{route.element}</Layout>
+//                   )
+//                 }
+//                 key={idx}
+//               />
+//             </>
+//           ))}
+//         </Route>
+//       </Routes>
+//     </React.Fragment>
+//   );
+// };
+
+// export default AllRoutes;
