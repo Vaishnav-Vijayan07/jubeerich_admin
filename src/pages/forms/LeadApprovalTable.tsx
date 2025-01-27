4c1650586f8280afd1353c0e7f26412a2b4939a6
Joshua-Intersmart <Joshua@intersmart.in>
2025-01-27 13:55:43 +0530

    delete added
    delete added

--- src/pages/forms/LeadApprovalTable.tsx
+++ src/pages/forms/LeadApprovalTable.tsx
@@ -7,7 +7,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import RefreshConfirmation from '../../components/RefreshConfirmation';
 import axios from 'axios';
 import { GridApi, GridReadyEvent, IRowNode, RowNode } from 'ag-grid-community';
-import { showSuccessAlert } from '../../constants';
+import { showErrorAlert, showSuccessAlert } from '../../constants';
+import { withSwal } from "react-sweetalert2";
 
 interface IRowData {
   id: number;
@@ -24,11 +25,11 @@ interface IRowData {
   error: string | null;
 }
 
-const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: any) => {
+const LeadApprovalTable = withSwal(({ swal, isOpenModal, toggleModal, responseData, options }: any) => {
+  const [gridApi, setGridApi] = useState<GridApi | null>(null);
   const [rowData, setRowData] = useState<any[]>([]);
   const [selectedItems, setSelectedItems] = useState<any[]>([]);
   const pageSizes = [10, 20, 50, 100];
-  const [gridApi, setGridApi] = useState<GridApi | null>(null);
   const [columnDefs, setColumnDefs] = useState<any[]>([]);
 
   const formattedData = useMemo(() => {
@@ -58,6 +59,7 @@ const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: 
         sortable: true,
         filter: true,
         editable: false,
+        width: 10,
         valueGetter: (params: any) => params.node.rowIndex + 1,
       },
       {
@@ -193,26 +195,26 @@ const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: 
           'cell-error': (params: any) => !params.value,
         },
       },
-      { field: 'error', headerName: 'Error', sortable: true, filter: true, editable: true, sortIndex: 0, sort: 'desc'},
-      // {
-      //   field: 'delete',
-      //   headerName: 'Actions',
-      //   cellRenderer: (params: any) => {
-      //     return (
-      //       <span>
-      //         <i onClick={() => {
-      //           const index = params.node.rowIndex;
-      //           handleDeleteRow(index, rowData);
-      //         }}
-      //           className='mdi mdi-delete-outline fs-4'></i>
-      //       </span>
-      //     );
-      //   },
-      //   width: 100,
-      //   sortable: false,
-      //   filter: false,
-      //   editable: false
-      // }
+      { field: 'error', headerName: 'Error', sortable: true, filter: true, editable: true },
+      {
+        field: 'delete',
+        headerName: 'Actions',
+        cellRenderer: (params: any) => {
+          return (
+            <span>
+              <i onClick={() => {
+                const index = params.node.rowIndex;
+                const allRowsData = params.api.getRenderedNodes().map((node: any) => node.data);
+                handleDeleteRow(index, allRowsData);
+              }}
+                className='mdi mdi-delete-outline fs-4'></i>
+            </span>
+          );
+        },
+        sortable: false,
+        filter: false,
+        editable: false
+      }
     ])
   }
 
@@ -223,20 +225,42 @@ const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: 
   };
 
   const handleApproved = async () => {
-    console.log('selectedItems', selectedItems);
-    const currentData = getCurrentTableData();
-    console.log('currentData', currentData);
-
+    if (!selectedItems || selectedItems.length == 0) {
+      showErrorAlert('No leads selected. Please select leads to approve.');
+      return;
+    }
+  
+    const hasInvalidItem = selectedItems.some((item) => {
+      return (
+        !item.source || 
+        !item.channel || 
+        !item.office_type || 
+        !item.email || 
+        !item.phone ||
+        !item.full_name ||
+        item.errors
+      );
+    });
+  
+    if (hasInvalidItem) {
+      showErrorAlert(
+        'Some selected leads are missing required fields: Source, Channels, OfficeType, Email, or Phone.'
+      );
+      return;
+    }
+  
     try {
       const { data } = await axios.post('/approve_leads', { lead_data: selectedItems });
       console.log('Response', data);
-      if(data) {
-        showSuccessAlert('Selected Leads Succesfully Approved')
+      if (data) {
+        showSuccessAlert('Selected Leads Successfully Approved');
+        // toggleModal(false);
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      showErrorAlert('An error occurred while approving leads.');
     }
-  }
+  };
 
   const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -246,33 +270,51 @@ const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: 
   ) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
-
+  
   const onGridReady = useCallback((params: GridReadyEvent) => {
     setGridApi(params.api);
   }, []);
 
-  const getCurrentTableData = useCallback(() => {
+  const getCurrentTableData = () => {
     if (!gridApi) return [];
 
     const currentData: any[] = [];
-    gridApi.forEachNode((rowNode: IRowNode, index: number) => {
-      currentData.push(rowNode.data);
+    const renderedNodes = gridApi.getRenderedNodes();
+    renderedNodes.forEach((node) => {
+      currentData.push(node.data);
     });
 
     return currentData;
-  }, [gridApi]);
+  };
 
-  const handleDeleteRow = (index: number, data: any) => {
-    console.log('index', index);
-    const currentData = getCurrentTableData();
-    console.log('currentData', currentData);
+  const handleDeleteRow = async(index: number, data: any) => {
+    // const result = await swal.fire({
+    //   title: "Confirm Action",
+    //   text: `Do you want to save the changes?`,
+    //   icon: "question",
+    //   iconColor: "#8B8BF5", // Purple color for the icon
+    //   showCancelButton: true,
+    //   confirmButtonText: `Yes, Save`,
+    //   cancelButtonText: "Cancel",
+    //   confirmButtonColor: "#8B8BF5", // Purple color for confirm button
+    //   cancelButtonColor: "#E97777", // Pink/red color for cancel button
+    //   buttonsStyling: true,
+    //   customClass: {
+    //     popup: "rounded-4 shadow-lg",
+    //     confirmButton: "btn btn-lg px-4 rounded-3 order-2 hover-custom",
+    //     cancelButton: "btn btn-lg px-4 rounded-3 order-1 hover-custom",
+    //     title: "fs-2 fw-normal mb-2",
+    //   },
+    //   width: "26em",
+    //   padding: "2em",
+    // });
 
-    const updatedData = currentData.filter((_, i) => i != index);
-    console.log('updatedData', updatedData);
-    // setRowData(updatedData);
-
-    // if(rowData.length && updatedData.length) {
-    //   setRowData(updatedData);
+    // if (result.isConfirmed) {
+      const currentData = getCurrentTableData();
+      const updatedData = data.filter((_: any, i: any) => i != index);
+      if (updatedData.length) {
+        setRowData(updatedData);
+      }
     // }
   };
 
@@ -282,7 +324,7 @@ const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: 
         fullScreen
         open={isOpenModal}
         onClose={() => toggleModal(false)}
-      // TransitionComponent={Transition}
+        // TransitionComponent={Transition}
       >
         <AppBar sx={{ position: 'relative' }}>
           <Toolbar>
@@ -296,39 +338,46 @@ const LeadApprovalTable = ({ isOpenModal, toggleModal, responseData, options }: 
             </IconButton>
           </Toolbar>
         </AppBar>
-        <div className='ag-theme-alpine' style={{ marginTop: '1rem', height: 450, width: '100%' }}>
-          <AgGridReact
-            rowData={rowData}
-            columnDefs={columnDefs}
-            pagination={true}
-            paginationPageSize={10}
-            defaultColDef={{
-              resizable: true,
-              flex: 1,
-            }}
-            theme={defaultTheme}
-            animateRows={true}
-            onSelectionChanged={onSelectionChanged}
-            loadThemeGoogleFonts={true}
-            suppressAutoSize={true}
-            rowSelection={{
-              enableClickSelection: false,
-              mode: 'multiRow'
-            }}
-            paginationPageSizeSelector={pageSizes}
-            onGridReady={onGridReady}
-          />
-          <div className='d-flex justify-content-end me-2 mt-2'>
-            <button className='btn btn-success' onClick={handleApproved}>
+        <div className="d-flex flex-column" style={{ height: '100vh' }}>
+          {/* AG Grid Container */}
+          <div
+            className="ag-theme-alpine flex-grow-1"
+            style={{ marginTop: '1rem', height: 'auto', width: '100%' }}
+          >
+            <AgGridReact
+              rowData={rowData}
+              columnDefs={columnDefs}
+              pagination={true}
+              paginationPageSize={10}
+              defaultColDef={{
+                resizable: true,
+                flex: 1,
+              }}
+              theme={defaultTheme}
+              animateRows={true}
+              onSelectionChanged={onSelectionChanged}
+              loadThemeGoogleFonts={true}
+              suppressAutoSize={true}
+              rowSelection={{
+                enableClickSelection: false,
+                mode: 'multiRow',
+              }}
+              paginationPageSizeSelector={pageSizes}
+              onGridReady={onGridReady}
+            />
+          </div>
+  
+          {/* Button Container */}
+          <div className="d-flex justify-content-end p-2">
+            <button className="btn btn-success" onClick={handleApproved}>
               Approve
             </button>
           </div>
         </div>
       </Dialog>
-
-      {/* {responseData && <RefreshConfirmation />} */}
     </>
-  )
-}
+  );
+  
+})
 
 export default LeadApprovalTable
